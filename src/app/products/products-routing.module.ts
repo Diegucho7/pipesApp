57c82be33e84f2bc4e8f634827786dc790856268
Diego <dvelarde140@gmail.com>
2023-08-04 16:53:13 -0500

    problema solucionado

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BasicsPageComponent } from '../pages/basics-page/basics-page.component';
-import { NumbersPageComponent } from '../pages/numbers-page/numbers-page.component';
-import { UncommonPagePageComponent } from '../pages/uncommon-page-page/uncommon-page-page.component';
+import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
+import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { UncommonPagePageComponent } from './pages/uncommon-page-page/uncommon-page-page.component';
 
 const routes: Routes = [
   {
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
+import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { UncommonPagePageComponent } from './pages/uncommon-page-page/uncommon-page-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BasicsPageComponent,
+    NumbersPageComponent,
+    UncommonPagePageComponent
+  ],
   imports: [
     CommonModule,
     ProductsRoutingModule,
