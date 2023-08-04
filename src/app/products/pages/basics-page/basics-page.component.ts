e72bb500218410cdfa2b13bc6c4d2a9060fb0722
Diego <dvelarde140@gmail.com>
2023-08-04 17:24:11 -0500

    continuacion de ejecicio funcional

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./basics-page.component.css']
 })
 export class BasicsPageComponent {
-
+  public nameLower: string = 'diego';
+  public nameUpper: string = 'DIEGO';
+  public fullName: string = 'dIeGo VeLaRdE';
 }
