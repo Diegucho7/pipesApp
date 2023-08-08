2a777b6b9c6b09c2ecc1887b0044c96a65e8b38c
Diegucho7 <diegucho7t@gmail.com>
2023-08-07 22:05:01 -0500

    avance corto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -29,7 +29,11 @@ registerLocaleData(localEsEc);
     MenubarModule
 
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'es-EC'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
