5ed774fb2f036a88bdc02b3c9463514912fea61e
Diego <dvelarde140@gmail.com>
2023-08-08 14:42:10 -0500

    avance en number pipes

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,16 +4,21 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
+import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { UncommonPagePageComponent } from './pages/uncommon-page-page/uncommon-page-page.component';
 
 
 @NgModule({
   declarations: [
-    BasicsPageComponent
+    BasicsPageComponent,
+    NumbersPageComponent,
+    UncommonPagePageComponent
   ],
   imports: [
     CommonModule,
     ProductsRoutingModule,
-    PrimeNgModule
+    PrimeNgModule,
+    
   ]
 })
 export class ProductsModule { }
