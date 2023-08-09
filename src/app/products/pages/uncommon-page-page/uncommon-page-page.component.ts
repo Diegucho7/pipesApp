9c65b7c1da9a8b4f29b8c6084ee40fa840265204
Diego <dvelarde140@gmail.com>
2023-08-09 14:53:37 -0500

    Avance en Pipes, completos

--- src/app/products/pages/uncommon-page-page/uncommon-page-page.component.ts
+++ src/app/products/pages/uncommon-page-page/uncommon-page-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page-page',
@@ -23,7 +24,31 @@ export class UncommonPagePageComponent {
 
   // i18n Plural
   public clients: string[] = ['Maria','Pedro', 'Fernando','Mario','Eduardo','Melissa', 'Natalia']
+  public clientMap = {
+    '=0':'no tenemos ningún cliente esperando',
+    '=1':'tenemos un cliente esperando',
+    '=2':'tenemos 2 clientes esperando',
+    'other':'tenemos # clientes esperando'
+
+  }
   deleteClient():void{
     this.clients.shift();
   }
+  public person = {
+    name: 'Diego',
+    age: 29,
+    address: 'Ottawa, Canada',
+  }
+  //Async Pipe
+  public myObservableTime: Observable<number> = interval(2000).pipe(
+    tap(value => console.log('tap:',value))
+  );
+  public promiseValue: Promise<string> = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      resolve('Tenemos data en la promesa.');
+      console.log('Tenemos data en la promesa.');
+      this.person.name='Otro nombre';
+    },3500)
+  })
+
 }
